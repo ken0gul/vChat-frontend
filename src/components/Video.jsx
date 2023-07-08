9674bec5a7407b4bce89120ead1b3154ed7cc050
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-07-08 17:29:45 -0400

    m

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Video = ({ myRef }) => {
-  return <video playsInline muted ref={myRef} autoPlay className="video" />;
+  return <video playsInline ref={myRef} autoPlay className="video" />;
 };
 export default Video;
